f617142e6efe2b98d76f9d9e48bda2c59a58fd39
Ben Reyes <benstudying2468@gmail.com>
2021-09-13 16:41:36 -0500

    Player Object Created
    Player object was created and a simple script for movement was started; the script is still incomplete.

---
+++ ReyesShootingGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+/*****
+ * Created by: Ben Reyes
+ * Date Created: Sept 13, 2021
+ * 
+ * Last Edited by: Sept 13, 2021
+ * Last Updated by: Sept 13, 2021
+ * 
+ * Description: Player control movements
+ */
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
